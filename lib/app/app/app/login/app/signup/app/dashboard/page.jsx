d44e8734e951c2ab4f5180edea808f4ae323366c
tycon5555 <165697903+tycon5555@users.noreply.github.com>
2026-01-28 11:52:27 -0800

    Create page.jsx

---
+++ lib/app/app/app/login/app/signup/app/dashboard/page.jsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <h1 className="text-3xl font-bold text-white">
+        Welcome to your Dashboard!
+      </h1>
+    </div>
+  );
+}
